d622d87f4a9207b60ad946618b5896e2803a761f
Tony <tony@gmail.com>
2021-03-07 11:46:49 +0800

    whole commit

--- app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 
-class SunnyWeahterApplication : Application(){
+class SunnyWeatherApplication : Application(){
     companion object{
         const val TOKEN = "MhCRtaYV7GLRMpky"
 
--- app/src/main/java/com/sunnyweather/android/convert.kt
+++ app/src/main/java/com/sunnyweather/android/convert.kt
@@ -1,7 +1,15 @@
 package com.sunnyweather.android
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
 fun main(){
-    val str = "C:\\Users\\11525\\AndroidStudioProjects\\SunnyWeather"
+//    GlobalScope.launch {
+//        println("codes run in coroutine scope")
+//    }
+//    Thread.sleep(1000)
+
+    val str = "app\\src\\main\\java\\com\\sunnyweather\\android\\logic\\model"
     println(str.replace("\\","/"))
 
 }
